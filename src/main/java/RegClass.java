8d23ca088c936b1887af1ad71024173558416b75
Eugene753 <Imarkadanov@gmail.com>
2021-11-04 14:57:22 -0400

    RegClass_changed

--- src/main/java/RegClass.java
+++ src/main/java/RegClass.java
@@ -3,11 +3,15 @@ public class RegClass {
     int a;
     int b;
     String str1;
+    int c;
+    int d;
 
-    public RegClass(int a,int b,String str1){
+    public RegClass(int a,int b,String str1,int c,int d){
         a=this.a;
         b=this.b;
         str1=this.str1;
+        c=this.c;
+        d=this.d;
     }
 
 
