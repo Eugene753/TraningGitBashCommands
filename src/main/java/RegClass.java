6f892f2df0259d2a4ccfe3e75d79efe645c8042b
Eugene753 <Imarkadanov@gmail.com>
2021-11-04 14:49:21 -0400

    My Commit

---
+++ src/main/java/RegClass.java
@@ -0,0 +1,15 @@
+public class RegClass {
+
+    int a;
+    int b;
+    String str1;
+
+    public RegClass(int a,int b,String str1){
+        a=this.a;
+        b=this.b;
+        str1=this.str1;
+    }
+    
+
+
+}
