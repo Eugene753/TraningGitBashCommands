34ea17fe7ec0ff1452cc32fc1c2ab4ef9573b754
Eugene753 <Imarkadanov@gmail.com>
2021-11-04 15:15:18 -0400

    Commit 2

--- src/main/java/RegClass.java
+++ src/main/java/RegClass.java
@@ -6,14 +6,18 @@ public class RegClass {
     int c;
     int d;
     String str2;
+    long l1;
+    long l2;
 
-    public RegClass(int a,int b,String str1,int c,int d,String str2){
+    public RegClass(int a,int b,String str1,int c,int d,String str2, long l1,long l2){
         a=this.a;
         b=this.b;
         str1=this.str1;
         c=this.c;
         d=this.d;
         str2=this.str2;
+        l1=this.l1;
+        l2=this.l2;
     }
 
 
