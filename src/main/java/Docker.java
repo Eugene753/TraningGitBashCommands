f02d94296bf9996e1365ac5ddc709a16075cc9e5
Eugene753 <Imarkadanov@gmail.com>
2021-11-04 15:12:58 -0400

    My commit

--- src/main/java/Docker.java
+++ src/main/java/Docker.java
@@ -4,11 +4,15 @@ public class Docker {
    int num2;
    int num3;
    int num4;
+   int num5;
+   int num6;
 
-   public Docker(int num1,int num2,int num3,int num4){
+   public Docker(int num1,int num2,int num3,int num4,int num5,int num6){
        num1=this.num1;
        num2=this.num2;
        num3=this.num3;
        num4=this.num4;
+       num5=this.num5;
+       num6=this.num6;
    }
 }
